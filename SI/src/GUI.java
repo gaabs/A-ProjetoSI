11546e17787033aacacc79474fd8b51da89ea68f
gaabs <gaabs@cin.ufpe.br>
2015-11-11 11:02:26 -0300

    Adicionei booleanos obstaculo, start, end, fronteira, em expansao na classe Square. Adicionei metodos de pintar o Square de acordo com os booleanos.

--- SI/src/GUI.java
+++ SI/src/GUI.java
@@ -7,6 +7,7 @@ import java.awt.event.MouseListener;
 import java.util.Scanner;
 
 import javax.swing.ButtonGroup;
+import javax.swing.ButtonModel;
 import javax.swing.JRadioButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -16,19 +17,19 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 
 
-public class Teste {
+public class GUI {
 
 	private JFrame frame;
 	private JPanel panel;
 	private JPanel menu;
-	private JPanel grid;
+	private JPanel gridPanel;
 	private JRadioButton startButton;
-	private JRadioButton goalButton;
+	private JRadioButton endButton;
 	private JRadioButton obstaclesButton;
-	private boolean[][] moveable;
 	private JLabel wValueLabel;
 	private JTextField wValueTextField;
-	private Square start, goal;
+	private Square startSquare, endSquare;
+	private Square[][] grid;
 
 	/**
 	 * Launch the application.
@@ -37,7 +38,7 @@ public class Teste {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Teste window = new Teste();
+					GUI window = new GUI();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -49,7 +50,7 @@ public class Teste {
 	/**
 	 * Create the application.
 	 */
-	public Teste() {
+	public GUI() {
 		initialize();
 	}
 
@@ -63,98 +64,123 @@ public class Teste {
 		panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
-		
+
 		menu = new JPanel();
 		menu.setLocation(0, 0);
 		panel.add(menu);
 		menu.setSize(784,77);
-		
+
 		startButton = new JRadioButton("Set start location");
 		startButton.setActionCommand("start");
 		startButton.setBounds(184, 5, 134, 23);
-		goalButton = new JRadioButton("Set goal location");
-		goalButton.setActionCommand("goal");
-		goalButton.setBounds(320, 5, 135, 23);
+		endButton = new JRadioButton("Set end location");
+		endButton.setActionCommand("end");
+		endButton.setBounds(320, 5, 135, 23);
 		obstaclesButton = new JRadioButton("Set obstacles");
 		obstaclesButton.setActionCommand("obstacle");
 		obstaclesButton.setBounds(457, 5, 116, 23);
 		menu.setLayout(null);
 		menu.add(startButton);
-		
+
 		wValueLabel = new JLabel("w value:");
 		wValueLabel.setBounds(236, 52, 46, 14);
 		menu.add(wValueLabel);
-		menu.add(goalButton);
+		menu.add(endButton);
 		menu.add(obstaclesButton);
 		ButtonGroup buttonGroup = new ButtonGroup();
 		buttonGroup.add(startButton);
-		buttonGroup.add(goalButton);
+		buttonGroup.add(endButton);
 		buttonGroup.add(obstaclesButton);
-		
+		obstaclesButton.setSelected(true);
+
 		wValueTextField = new JTextField();
 		wValueTextField.setText("1.0");
 		wValueTextField.setBounds(292, 48, 54, 23);
 		menu.add(wValueTextField);
 		wValueTextField.setColumns(10);
-		
+
 		JButton SearchButton = new JButton("Search");
 		SearchButton.setBounds(411, 48, 89, 23);
 		menu.add(SearchButton);
-		
+
 		//frame.getContentPane().add(menu, BorderLayout.NORTH);
-		
+
 		int n = 10;
 		Scanner scan = new Scanner(System.in);
 		//n = scan.nextInt();
-		grid = new JPanel();
-		grid.setBounds(0, 78, 784, 483);
-		panel.add(grid);
-		grid.setLayout(new GridLayout(n,n));
-		
-		
-		moveable = new boolean[n][n];
+		gridPanel = new JPanel();
+		gridPanel.setBounds(0, 78, 784, 483);
+		panel.add(gridPanel);
+		gridPanel.setLayout(new GridLayout(n,n));
+
+
+		grid = new Square[n][n];
 		for(int i = 0; i < n; i++){
 			for(int j = 0; j < n; j++){
 				Square square = new Square(i,j);
 				square.addMouseListener(new MouseListener() {
-					
+
 					@Override
 					public void mouseReleased(MouseEvent e) {
 						// TODO Auto-generated method stub
-						
+
 					}
-					
+
 					@Override
 					public void mousePressed(MouseEvent e) {
-						moveable[square.i][square.j] = !moveable[square.i][square.j];
-						
-						if (moveable[square.i][square.j]) square.setBackground(Color.WHITE);
-						else square.setBackground(Color.GRAY);
-						
+						//moveable[square.i][square.j] = !moveable[square.i][square.j];
+
 						System.out.println(buttonGroup.getSelection().getActionCommand());
+						String choice = buttonGroup.getSelection().getActionCommand();
+						if (choice == "start"){
+							if (startSquare != null){
+								startSquare.start = false;
+								startSquare.repaint();
+							} 
+							startSquare = square;
+							square.start = true;
+							square.repaint();
+						}
+						
+						if (choice == "end"){
+							if (endSquare != null){
+								endSquare.end = false;
+								endSquare.repaint();
+							} 
+							endSquare = square;
+							square.end = true;
+							square.repaint();
+						}
+
+						if (choice == "obstacle"){
+							if (!square.start && !square.end){
+								square.obstacle = !square.obstacle;
+								square.repaint();
+							}
+						}
 					}
-					
+
 					@Override
 					public void mouseExited(MouseEvent e) {
 						// TODO Auto-generated method stub
-						
+
 					}
-					
+
 					@Override
 					public void mouseEntered(MouseEvent e) {
 						// TODO Auto-generated method stub
-						
+
 					}
-					
+
 					@Override
 					public void mouseClicked(MouseEvent e) {
 						// TODO Auto-generated method stub
-						
+
 					}
 				});
-				
-				grid.add(square);
-				moveable[i][j] = true;
+
+				gridPanel.add(square);
+				grid[i][j] = square;
 			}
 		}
 		//frame.pack();
--- SI/src/Square.java
+++ SI/src/Square.java
@@ -1,11 +1,12 @@
 import java.awt.Color;
-
+import java.awt.Graphics;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 
 public class Square extends JPanel {
 	int i,j;
+	boolean obstacle, start, end, border, expanding;
 
 	/**
 	 * Create the panel.
@@ -13,10 +14,33 @@ public class Square extends JPanel {
 	public Square(int i, int j) {
 		this.i = i;
 		this.j = j;
+		obstacle = start = end = border = expanding = false;
 		
 		this.setBackground(Color.WHITE);
 		this.setBorder(BorderFactory.createLineBorder(Color.black));
 		this.setToolTipText(String.format("(%d,%d)",i,j));
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		if (start) drawStart(g);
+		if (end) drawEnd(g);
+		
+		if (expanding) setBackground(Color.YELLOW);
+		else if (border) setBackground(Color.ORANGE);	
+		else if (!obstacle) setBackground(Color.WHITE);
+		else setBackground(Color.GRAY);
+	}
+	
+	public void drawStart(Graphics g){
+		g.setColor(Color.RED);
+		g.fillOval(0, 0, 10, 10);
+	}
+	
+	public void drawEnd(Graphics g){
+		g.setColor(Color.GREEN);
+		g.fillOval(this.getWidth()-10, 0, 10, 10);
 	}
 
 }
